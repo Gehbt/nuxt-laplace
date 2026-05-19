e9b239f82adba7f515d061f542b56de6bb983368
rg <rgehbt@gmail.com>
2026-05-19 17:51:39 +0800

    feat: add server plugin to seed default chat rooms

---
+++ server/plugins/seed.ts
@@ -0,0 +1,7 @@
+export default defineNitroPlugin(async () => {
+  const rooms = await getRooms()
+  if (rooms.length === 0) {
+    await createRoom('General')
+    await createRoom('Random')
+  }
+})
