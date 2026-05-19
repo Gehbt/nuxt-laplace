2b195ab16c760ae97310a64b0c0c03139e8f6890
rg <rgehbt@gmail.com>
2026-05-19 18:03:07 +0800

    feat: add chat pages with blank layout and room navigation

---
+++ app/pages/chat/[roomId].vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+definePageMeta({ layout: 'blank' })
+
+const route = useRoute()
+const roomId = computed(() => route.params.roomId as string)
+
+const store = useChatStore()
+const { joinRoom, sendMessage, sendTyping, createRoom } = useChat()
+
+watch(
+  roomId,
+  (newRoomId) => {
+    if (newRoomId) {
+      joinRoom(newRoomId)
+    }
+  },
+  { immediate: true },
+)
+</script>
+
+<template>
+  <div class="h-screen flex bg-white dark:bg-gray-900">
+    <ChatRoomSidebar
+      :rooms="store.rooms"
+      :current-room-id="store.currentRoomId"
+      :online-users="store.currentOnlineUsers"
+      @select-room="(id: string) => navigateTo(`/chat/${id}`)"
+      @create-room="createRoom"
+    />
+    <div class="flex-1 flex flex-col">
+      <div class="px-4 py-3 border-b border-gray-200 dark:border-gray-700 font-semibold">
+        # {{ store.currentRoomId }}
+        <span v-if="!store.connected" class="ml-2 text-sm text-red-500 font-normal">
+          (Connecting...)
+        </span>
+      </div>
+      <ChatMessageList
+        :messages="store.currentMessages"
+        :current-peer-id="store.peerId"
+        :typing-peer-id="store.typingPeerId"
+      />
+      <ChatMessageInput @send="sendMessage" @typing="sendTyping" />
+    </div>
+  </div>
+</template>
